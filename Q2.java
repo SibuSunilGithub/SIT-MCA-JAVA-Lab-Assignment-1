76f2a059bd4de87821fc6e476d7d30e41382e391
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2024-02-17 22:25:33 +0530

    Arithmetic Operations In Java
    Write a Java Program to perform different arithmetic operations between 
    two numbers.

---
+++ Q2.java
@@ -0,0 +1,10 @@
+class Q2{
+	public static void main(String args[]){
+		int num1 = 10, num2 = 20;
+		System.out.println("Addition "+(num1+num2));
+		System.out.println("Subtraction "+(num1-num2));
+		System.out.println("Multiplication "+(num1*num2));
+		System.out.println("Division "+(num1/num2));
+		System.out.println("Modolus "+(num1%num2));
+	}
+}
