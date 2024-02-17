242755ef97f5f43c635d9947e9c9b5b67fccb222
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2024-02-17 22:26:16 +0530

    Check Divided By 5
    Write a Java Program to check if a number is divisible by 5.

---
+++ Q3.java
@@ -0,0 +1,13 @@
+class Q3{
+	public static void main(String args[]){
+		int num = 15;
+		if(num % 5 == 0)
+		{
+			System.out.println("Divisible");
+		}
+		else
+		{
+			System.out.println("Not Divisible");
+		}
+	}
+}
