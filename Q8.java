d29afba8273b9fd555a80122b59065f5a2d67763
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2024-05-28 20:04:50 +0530

    Multiplication Table Display
    Write a Java Program to display a multiplication table of any number.

---
+++ Q8.java
@@ -0,0 +1,10 @@
+// Write a Java Program to display a multiplication table of any number.
+
+class Q8 {
+	public static void main(String args[]) {
+		int num = 5, i;
+		for (i = 1; i <= 10; i++) {
+			System.out.println(num + " X " + i + " = " + (num * i));
+		}
+	}
+}
